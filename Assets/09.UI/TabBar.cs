72aa6a2f580a0f66dfbeb5ccac3b25ae87eefc02
kainel54 <kainel0504@gmail.com>
2023-10-14 20:05:00 +0900

    Tapbar 만들기

--- Assets/09.UI/TabBar.cs
+++ Assets/09.UI/TabBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,5 +10,26 @@ public class TabBar : MonoBehaviour
     private void Awake()
     {
         uiDocument = GetComponent<UIDocument>();
+    }
+
+    private void OnEnable()
+    {
+        var root = uiDocument.rootVisualElement;
+        var slideBox = root.Q("pages");
+        root.Q<Label>("home").RegisterCallback<ClickEvent>(e =>
+        {
+            Debug.Log("home");
+            slideBox.style.left = new Length(0, LengthUnit.Percent);
+        });
+        root.Q<Label>("music").RegisterCallback<ClickEvent>(e =>
+        {
+            Debug.Log("music");
+            slideBox.style.left = new Length(-100, LengthUnit.Percent);
+        });
+        root.Q<Label>("game").RegisterCallback<ClickEvent>(e =>
+        {
+            Debug.Log("game");
+            slideBox.style.left = new Length(-200, LengthUnit.Percent);
+        });
     }
 }
